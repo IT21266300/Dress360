fcfcec32b426918c47ba52e256eafeec916efa84
Vihinsa Thejan Bandara <88578813+IT21266300@users.noreply.github.com>
2024-02-28 21:57:44 +0530

    add backend startup files
    add backend startup files with dependencies:::::: Spring Web WEB
    Build web, including RESTful, applications using Spring MVC. Uses Apache Tomcat as the default embedded container.
    Spring Data JPA SQL
    Persist data in SQL stores with Java Persistence API using Spring Data and Hibernate.
    Spring Security SECURITY
    Highly customizable authentication and access-control framework for Spring applications.
    Config Client SPRING CLOUD CONFIG
    Client that connects to a Spring Cloud Config Server to fetch the application's configuration.
    OpenFeign SPRING CLOUD ROUTING
    Declarative REST Client. OpenFeign creates a dynamic implementation of an interface decorated with JAX-RS or Spring MVC annotations.
    Azure OpenAI AI
    Spring AI support for Azure’s OpenAI offering, powered by ChatGPT. It extends beyond traditional OpenAI capabilities, delivering AI-driven text generation with enhanced functionality.
    Azure Storage MICROSOFT AZURE
    All Storage features are supported, e.g. blob, fileshare and queue.

---
+++ project-drees360/Backend/project-drees360/src/main/java/com/dress360/projectdrees360/ProjectDrees360Application.java
@@ -0,0 +1,13 @@
+package com.dress360.projectdrees360;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectDrees360Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectDrees360Application.class, args);
+	}
+
+}
---
+++ project-drees360/Backend/project-drees360/src/test/java/com/dress360/projectdrees360/ProjectDrees360ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dress360.projectdrees360;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectDrees360ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
