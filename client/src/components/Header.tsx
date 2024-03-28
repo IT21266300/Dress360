9f933a4740239394ddae0de07601a81c51e32772
Dumindu Bandara <lakshand969@gmail.com>
2024-03-28 19:58:34 +0530

    "Replace HideOnScroll with ScrollTop and update functionality and styling"

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -6,133 +6,65 @@ import CssBaseline from '@mui/material/CssBaseline';
 import useScrollTrigger from '@mui/material/useScrollTrigger';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
-import Slide from '@mui/material/Slide';
-import { Link } from 'react-router-dom';
-import { Badge, Button, IconButton } from '@mui/material';
-import ShoppingBagIcon from '@mui/icons-material/ShoppingBag';
-import AccountCircleIcon from '@mui/icons-material/AccountCircle';
-import { colorPalette } from '../theme';
+import Fab from '@mui/material/Fab';
+import KeyboardArrowUpIcon from '@mui/icons-material/KeyboardArrowUp';
+import Fade from '@mui/material/Fade';
 
 interface Props {
-  /**
-   * Injected by the documentation to work in an iframe.
-   * You won't need it on your project.
-   */
   window?: () => Window;
   children: React.ReactElement;
 }
 
-function HideOnScroll(props: Props) {
+function ScrollTop(props: Props) {
   const { children, window } = props;
-  // Note that you normally won't need to set the window ref as useScrollTrigger
-  // will default to window.
-  // This is only being set here because the demo is in an iframe.
   const trigger = useScrollTrigger({
     target: window ? window() : undefined,
+    disableHysteresis: true,
+    threshold: 100,
   });
 
+  const handleClick = (event: React.MouseEvent<HTMLDivElement>) => {
+    const anchor = (
+      (event.target as HTMLDivElement).ownerDocument || document
+    ).querySelector('#back-to-top-anchor');
+
+    if (anchor) {
+      anchor.scrollIntoView({
+        block: 'center',
+      });
+    }
+  };
+
   return (
-    <Slide appear={false} direction="down" in={!trigger}>
-      {children}
-    </Slide>
+    <Fade in={trigger}>
+      <Box
+        onClick={handleClick}
+        role="presentation"
+        sx={{ position: 'fixed', bottom: 16, right: 16 }}
+      >
+        {children}
+      </Box>
+    </Fade>
   );
 }
 
-export default function HideAppBar(props: Props) {
+export default function BackToTop(props: Props) {
   return (
     <React.Fragment>
       <CssBaseline />
-      <HideOnScroll {...props}>
-        <AppBar
-          sx={{
-            px: '2rem',
-            width: '100%',
-            height: '4rem',
-            textTransform: 'inherit',
-          }}
-        >
-          <Toolbar
-            sx={{
-              width: '100%',
-              height: '100%',
-              display: 'flex',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-            }}
-          >
-            <Box>
-              <Typography variant="h6" component="div">
-                Dress 360
-              </Typography>
-            </Box>
-            <Box
-              sx={{
-                fontSize: '1.3rem',
-                color: colorPalette.body[500],
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Button
-                sx={{
-                  color: colorPalette.body[500],
-                  textTransform: 'inherit',
-                  fontSize: '1rem',
-                }}
-                variant="text"
-                size="large"
-              >
-                Link
-              </Button>
-              <Button
-                sx={{
-                  color: colorPalette.body[500],
-                  textTransform: 'inherit',
-                  fontSize: '1rem',
-                }}
-                variant="text"
-                size="large"
-              >
-                Link
-              </Button>
-              <Button
-                sx={{
-                  color: colorPalette.body[500],
-                  textTransform: 'inherit',
-                  fontSize: '1rem',
-                }}
-                variant="text"
-                size="large"
-              >
-                Link
-              </Button>
-            </Box>
-            <Box
-              sx={{
-                height: '100%',
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}
-            >
-              <Button
-                sx={{ color: colorPalette.body[500], textTransform: 'inherit' }}
-                startIcon={<AccountCircleIcon />}
-                size="large"
-              >
-                Sign In
-              </Button>
-              <Badge badgeContent={4} color="secondary">
-                <IconButton size="small" sx={{ color: colorPalette.body[500] }}>
-                  <ShoppingBagIcon />
-                </IconButton>
-              </Badge>
-            </Box>
-          </Toolbar>
-        </AppBar>
-      </HideOnScroll>
-      <Toolbar />
+      <AppBar>
+        <Toolbar>
+          <Typography variant="h6" component="div">
+            Scroll to see button
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Toolbar id="back-to-top-anchor" />
+      <ScrollTop {...props}>
+        <Fab size="small" aria-label="scroll back to top">
+          <KeyboardArrowUpIcon />
+        </Fab>
+      </ScrollTop>
     </React.Fragment>
   );
 }
