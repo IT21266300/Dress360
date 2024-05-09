888285c010874bcb8fcc160cd1aba4d88c5d8329
Janith Kaushalya <it21268830@my.sliit.lk>
2024-05-09 19:46:23 +0530

    Update utils.js
    fixied backend error

--- Dress360/backend/utils.js
+++ Dress360/backend/utils.js
@@ -9,13 +9,8 @@ export function generateToken(user) {
     email: user.email,
     isAdmin: user.isAdmin,
   };
-  return sign(
-    {
-      _id: user._id,
-      name: user.name,
-      email: user.email,
-      isAdmin: user.isAdmin,
-    },
+  return jwt.sign(
+    payload,
     process.env.JWT_SECRET || 'somethingsecret',
     {
       expiresIn: '30d',
