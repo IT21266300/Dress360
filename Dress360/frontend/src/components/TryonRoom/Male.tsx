b99fb91fdc71804d65d3f4dabf5163d8c095c506
DhananjayaKumara99 <it21184208@my.sliit.lk>
2024-05-11 01:52:49 +0530

    succesfully import the male 3d model

---
+++ Dress360/frontend/src/components/TryonRoom/Male.tsx
@@ -0,0 +1,33 @@
+import * as THREE from 'three';
+import React from 'react';
+import { useGLTF } from '@react-three/drei';
+import * as Three from "three";
+
+type GLTFResult = {
+  nodes: {
+    Asset3DLoadersceneRoot: THREE.Mesh;
+  };
+  materials: Record<string, THREE.Material>;
+};
+
+
+
+export function Model(props: JSX.IntrinsicElements["group"]) {
+  const { nodes } = useGLTF("../../../blenderStuff/male.gltf") as any;
+
+  // Create a new material with dark gray color
+  const darkGrayMaterial = new THREE.MeshBasicMaterial({ color: "#343435" });
+
+  return (
+    <group {...props} dispose={null}>
+      {/* Apply the new material to the mesh */}
+      <mesh
+        geometry={(nodes.Asset3DLoadersceneRoot as THREE.Mesh).geometry}
+        material={darkGrayMaterial}
+        position={[0, -10, 0]} // Adjust the y-position to move the model downwards
+      />
+    </group>
+  );
+}
+
+useGLTF.preload('../../../blenderStuff/male.gltf');
--- Dress360/frontend/src/components/TryonRoom/TryonRoom.tsx
+++ Dress360/frontend/src/components/TryonRoom/TryonRoom.tsx
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
-// import close_icon from '../../assets/close.png';
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from "@react-three/drei";
+import { Model as MaleModel } from "./Male";
 import './TryonRoom.css';
 
-const TryonRoom = ({ handleClose, show }: { handleClose: any, show: any }) => {
+const TryonRoom = ({ handleClose, show }: { handleClose: any; show: any }) => {
+  const scale: [number, number, number] = [0.26, 0.26, 0.26]; // Adjust the scale factor for x, y, and z axes.
+  
   return (
     <Modal show={show} onHide={handleClose}>
       <Modal.Header closeButton>
         <Modal.Title>Try Your Clothes</Modal.Title>
       </Modal.Header>
-      <Modal.Body>
+      <Modal.Body className="tryon-room-body">
         <div className="tryon-room">
-          <div className="3d-model">
-            <div className="3d-model-left"></div>
-            <div className="3d-model-right"></div>
+          <div className="model-top">
+            <Canvas className='top-canva' camera={{ fov: 100, position: [0, 0, 3] }}>
+              <ambientLight intensity={5} />
+              <OrbitControls enableZoom={true} enablePan={false} enableRotate={true} maxPolarAngle={Math.PI / 2} />
+              <MaleModel scale={scale} />
+            </Canvas>
           </div>
+          <div className="model-bottom"></div>
         </div>
       </Modal.Body>
       <Modal.Footer>
@@ -24,6 +32,6 @@ const TryonRoom = ({ handleClose, show }: { handleClose: any, show: any }) => {
       </Modal.Footer>
     </Modal>
   );
-}
+};
 
 export default TryonRoom;
